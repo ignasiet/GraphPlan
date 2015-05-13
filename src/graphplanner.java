c3f09b4232f588c87870ab1d45be2727fdea962e
Ignasi <ignasi.af@gmail.com>
2015-05-13 17:36:12 -0300

    Added OR preconditions

--- src/graphplanner.java
+++ src/graphplanner.java
@@ -16,11 +16,15 @@ public class graphplanner {
 	private Hashtable<String, Integer> _State = new Hashtable<String, Integer>();
 	private ArrayList<String> _Goal = new ArrayList<String>();
 	protected ArrayList<String> _Plan = new ArrayList<String>();
+	protected Hashtable<Integer, ArrayList<String>> _ActionPlan = new Hashtable<Integer, ArrayList<String>>();
+	protected Integer last_layer = 0;
+	protected boolean fail = false;
 
 	public graphplanner(Hashtable<String, Integer> state, Hashtable<String, AbstractAction> Actions, ArrayList<String> goal) {
 		_State = state;
 		_Actions = Actions;
 		_Goal = goal;
+		Integer num_MAX_iter = 0;
 		initActionList();
 		Enumeration enumerator_states = state.keys();
 		Step stepInit = new Step();
@@ -31,14 +35,20 @@ public class graphplanner {
 			stepInit.addNode(no);
 		}
 		_Steps.add(stepInit);
-		while(!isGoal(_Steps.get(_Steps.size()-1))){
+		while(!isGoal(_Steps.get(_Steps.size()-1)) && num_MAX_iter < _Actions.size()){
 			//Expandimos o último nó
 			expandStep(_Steps.get(_Steps.size()-1));
+			num_MAX_iter++;
 		}
-		backtrackPlan();
-		System.out.println("=========================================");
-		System.out.println("Plano: ");
-		System.out.println(_Plan.toString());
+		if(num_MAX_iter == _Actions.size()){
+			System.out.println("FAIL! no plan found");
+			fail = true;
+		}else{
+			backtrackPlan();
+			System.out.println("=========================================");
+			System.out.println("Plano parcialmente ordenado (incluindo no-ops): ");
+			System.out.println(_Plan.toString());
+		}
 	}
 	
 	private void backtrackPlan() {
@@ -47,6 +57,7 @@ public class graphplanner {
 		ArrayList<String> _subgoal = new ArrayList<String>(_Goal);
 		while(currentStep.father != null){
 			ArrayList<String> _actualList = new ArrayList<String>();
+			ArrayList<String> _actions_Partial_ordered = new ArrayList<String>();
 			for(String p : _subgoal){
 				Node n = currentStep.getNode(p);
 				if(n.hasParent()){
@@ -54,6 +65,9 @@ public class graphplanner {
 						String new_Param = parent_node.predicate;
 						if(!achieved_preconds.containsKey(new_Param)){
 							_actualList.add(new_Param);
+							if(!new_Param.startsWith("No-op")){
+								_actions_Partial_ordered.add(new_Param);
+							}
 							achieved_preconds.put(new_Param, 1);
 						}
 					}					
@@ -63,11 +77,23 @@ public class graphplanner {
 			_subgoal.addAll(_actualList);
 			if((currentStep.step %2)==0){
 				_Plan.add(0, _actualList.toString());
+				_ActionPlan.put(currentStep.step -1 , _actions_Partial_ordered);
 			}
 			if(currentStep.father != null){
 				currentStep = currentStep.father;
 			}
 		}
+	}
+	
+	public Integer heuristicValue(){
+		int hValue = 0;
+		Enumeration e = _ActionPlan.keys();
+		while(e.hasMoreElements()){
+			Integer Key_Plan = Integer.parseInt(e.nextElement().toString());
+			ArrayList<String> partialActions = _ActionPlan.get(Key_Plan);
+			hValue += partialActions.size();
+		}
+		return hValue;
 	}
 
 	public void initActionList(){
@@ -96,8 +122,20 @@ public class graphplanner {
 				Node no = new Node(action_name);
 				ActionStep.addNode(no);
 				for(String precondition : a._precond){
-					predicates_list.updateSuccessorNode(precondition, no);
-					ActionStep.updateParentNode(action_name, predicates_list.getNode(precondition));
+					if(!precondition.contains("^")){						
+						predicates_list.updateSuccessorNode(precondition, no);
+						ActionStep.updateParentNode(action_name, predicates_list.getNode(precondition));
+					}
+					else{
+						//Action with or precondition
+						String[] orPrecond = precondition.split("\\^");
+						for(String orP : orPrecond){
+							if(predicates_list.Contains(orP)){
+								predicates_list.updateSuccessorNode(orP, no);
+								ActionStep.updateParentNode(action_name, predicates_list.getNode(orP));
+							}
+						}
+					}
 				}
 				for(String effect : a._Positive_effects){
 					Node node_effect = new Node(effect);
@@ -122,14 +160,29 @@ public class graphplanner {
 		_Steps.add(ActionStep);
 		_Steps.add(PredicateStep);
 		PredicateStep.step = predicates_list.step + 2;
-		ActionStep.step = ActionStep.step + 1;
+		ActionStep.step = predicates_list.step + 1;
+		last_layer = ActionStep.step;
 	}
 	
 	/**Verify if the action is applicable*/
 	private boolean isActionApplicable(AbstractAction a, Step s){
 		for(String precondition : a._precond){
-			if(!s.Contains(precondition)){
-				return false;
+			if(!precondition.contains("^")){
+				if(!s.Contains(precondition)){
+					return false;
+				}
+			}else{
+				boolean flag = false;
+				String[] orPrecond = precondition.split("\\^");
+				for(String orP : orPrecond){
+					if(s.Contains(orP)){
+						flag = true;
+						break;
+					}
+				}
+				if(!flag){
+					return false;
+				}
 			}
 		}
 		return true;
